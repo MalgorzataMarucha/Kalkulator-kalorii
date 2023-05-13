5e81e754375710d46ce5ff42e43951ed87b6b5b3
MalgorzataMarucha <92079121+MalgorzataMarucha@users.noreply.github.com>
2023-05-13 12:59:06 +0200

    Add files via upload

--- Kalkulator.py
+++ Kalkulator.py
@@ -3,6 +3,7 @@ from tkinter import ttk
 from tkinter import *
 import os
 import re
+import json
 
 class App(tk.Tk):
     def __init__(self):
@@ -24,6 +25,7 @@ class App(tk.Tk):
         file_path = os.path.join(os.path.abspath(os.path.dirname(__file__)), "forest-dark.tcl")
         self.tk.call("source", file_path)
         self.style.theme_use("forest-dark")
+        
     def menu(self):
 
         self.Makro = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]
@@ -36,43 +38,44 @@ class App(tk.Tk):
         treeScroll = ttk.Scrollbar(treeFrame)
         treeScroll.pack(side="right", fill="y")
 
-        treeview = ttk.Treeview(treeFrame, selectmode="extended", yscrollcommand=treeScroll.set, columns=(1, 2, 3, 4), height=5)
-        treeview.pack(expand=True, fill="both")
+        self.treeview = ttk.Treeview(treeFrame, selectmode="extended", yscrollcommand=treeScroll.set, columns=(1, 2, 3, 4), height=5)
+        self.treeview.pack(expand=True, fill="both")
     
-        treeScroll.config(command=treeview.yview)
+        treeScroll.config(command=self.treeview.yview)
 
         # Treeview columns
-        treeview.column("#0", width=round(self.width/3))
-        treeview.column(1, anchor="center", width=round(0.67*self.width/4.7))
-        treeview.column(2, anchor="center", width=round(0.67*self.width/4.7))
-        treeview.column(3, anchor="center", width=round(0.67*self.width/4.7))
-        treeview.column(4, anchor="center", width=round(0.67*self.width/4.7))
+        self.treeview.column("#0", width=round(self.width/3))
+        self.treeview.column(1, anchor="center", width=round(0.67*self.width/4.7))
+        self.treeview.column(2, anchor="center", width=round(0.67*self.width/4.7))
+        self.treeview.column(3, anchor="center", width=round(0.67*self.width/4.7))
+        self.treeview.column(4, anchor="center", width=round(0.67*self.width/4.7))
        
         # Treeview headings
-        treeview.heading("#0", text="Posiłek")
-        treeview.heading(1, text="Kcal")
-        treeview.heading(2, text="B")
-        treeview.heading(3, text="T")
-        treeview.heading(4, text="W")
+        self.treeview.heading("#0", text="Posiłek")
+        self.treeview.heading(1, text="Kcal")
+        self.treeview.heading(2, text="B")
+        self.treeview.heading(3, text="T")
+        self.treeview.heading(4, text="W")
 
         # Define treeview data
         treeview_data = [
-            ("", "end", 1, "Śniadanie", (str(self.Makro[1][0]), str(self.Makro[1][1]),str(self.Makro[1][2]),str(self.Makro[1][3]))),
-            ("", "end", 2, "Drugie Śniadanie", (str(self.Makro[2][0]), str(self.Makro[2][1]),str(self.Makro[2][2]),str(self.Makro[2][3]))),
-            ("", "end", 3, "Obiad", (str(self.Makro[3][0]), str(self.Makro[3][1]),str(self.Makro[3][2]),str(self.Makro[3][3]))),
-            ("", "end", 4, "Przekąska", (str(self.Makro[4][0]), str(self.Makro[4][1]),str(self.Makro[4][2]),str(self.Makro[4][3]))),
-            ("", "end", 5, "Kolacja", (str(self.Makro[5][0]), str(self.Makro[5][1]),str(self.Makro[5][2]),str(self.Makro[5][3]))),
+            ("", "end", 1, "Śniadanie", (self.Makro[1][0], self.Makro[1][1],self.Makro[1][2],self.Makro[1][3])),
+            ("", "end", 2, "Drugie Śniadanie", (self.Makro[2][0], self.Makro[2][1],self.Makro[2][2],self.Makro[2][3])),
+            ("", "end", 3, "Obiad", (self.Makro[3][0], self.Makro[3][1],self.Makro[3][2],self.Makro[3][3])),
+            ("", "end", 4, "Przekąska", (self.Makro[4][0], self.Makro[4][1],self.Makro[4][2],self.Makro[4][3])),
+            ("", "end", 5, "Kolacja", (self.Makro[5][0], self.Makro[5][1],self.Makro[5][2],self.Makro[5][3])),
         ]
 
         # Insert treeview data
         for item in treeview_data:
-            treeview.insert(parent=item[0], index=item[1], iid=item[2], text=item[3], values=item[4])
+            self.treeview.insert(parent=item[0], index=item[1], iid=item[2], text=item[3], values=item[4])
             if item[0] == "":
-                treeview.item(item[2], open=True) # Open parents
+                self.treeview.item(item[2], open=True) # Open parents
                 
         # Select and scroll
-        treeview.selection_set(2)
-        treeview.see(3)
+        self.treeview.selection_set(2)
+        self.treeview.see(3)
+        
 
         add_frame = ttk.LabelFrame(self, text="Dodaj posiłek")
         add_frame.place(x=0,y=590,width=500,height=110)
@@ -104,17 +107,29 @@ class App(tk.Tk):
         macro_frame = ttk.LabelFrame(self,text="Makro")
         macro_frame.place(x=250,y=0,width=250,height=170)
 
-        kcal_label = ttk.Label(macro_frame, text="Kcal: " + str(self.Makro[0][0]))
-        kcal_label.pack(side="top", padx=10, pady=10)
+        self.kcal_label = ttk.Label(macro_frame, text="Kcal: " + str(self.Makro[0][0]))
+        self.kcal_label.pack(side="top", padx=10, pady=10)
 
-        B_label = ttk.Label(macro_frame, text="Białko: " + str(self.Makro[0][1]))
-        B_label.pack(side="top", padx=10, pady=10)
+        self.B_label = ttk.Label(macro_frame, text="Białko: " + str(self.Makro[0][1]))
+        self.B_label.pack(side="top", padx=10, pady=10)
 
-        T_label = ttk.Label(macro_frame, text="Tłuszcze: " + str(self.Makro[0][2]))
-        T_label.pack(side="top", padx=10, pady=10)
+        self.T_label = ttk.Label(macro_frame, text="Tłuszcze: " + str(self.Makro[0][2]))
+        self.T_label.pack(side="top", padx=10, pady=10)
 
-        W_label = ttk.Label(macro_frame, text="Węglowodany: " + str(self.Makro[0][3]))
-        W_label.pack(side="top", padx=10, pady=10)
+        self.W_label = ttk.Label(macro_frame, text="Węglowodany: " + str(self.Makro[0][3]))
+        self.W_label.pack(side="top", padx=10, pady=10)
+
+    def update_macro_labels(self):
+        sum_kcal = sum(item[0] for item in self.Makro[1:])
+        sum_B = sum(item[1] for item in self.Makro[1:])
+        sum_T = sum(item[2] for item in self.Makro[1:])
+        sum_W = sum(item[3] for item in self.Makro[1:])
+
+        self.kcal_label.config(text="Kcal: " + str(sum_kcal))
+        self.B_label.config(text="Białko: " + str(sum_B))
+        self.T_label.config(text="Tłuszcze: " + str(sum_T))
+        self.W_label.config(text="Węglowodany: " + str(sum_W))
+        
     
     def whichButton(self,Button):
         if Button == 1:
@@ -138,7 +153,7 @@ class App(tk.Tk):
         new_window = tk.Toplevel(self)
         new_window.title("Szukaj produktu")
         new_window.geometry('%dx%d+%d+%d' % (self.width, self.height, self.x, self.y))
-        self.folder_path = os.path.join(os.path.dirname(__file__), "Dania")
+        self.folder_path = os.path.join(os.path.dirname(__file__), "Informacje")
         if not os.path.exists(self.folder_path):
             os.makedirs(self.folder_path)
         
@@ -152,15 +167,19 @@ class App(tk.Tk):
             for widget in result_frame.winfo_children():
                 widget.destroy()
             product = entry.get()
-            matching_files = [file for file in os.listdir(self.folder_path) if re.search(product, file, re.IGNORECASE)]
-            if matching_files:
-                for file in matching_files:
-                    label = ttk.Label(result_frame, text=file[:-4])
-                    label.pack(side="top", padx=10, pady=10)
-                    label.bind("<Button-1>", lambda event, new_window=new_window, file=file: self.AddProduct(event, new_window, file[:-4]))
-            else:
-                label = ttk.Label(result_frame, text="Brak wyników wyszukiwania. Dodaj produkt.")
-                label.pack(side="top", padx=10, pady=10)
+            file_path = os.path.join(self.folder_path, "Produkty.json")
+            if os.path.exists(file_path):
+                with open(file_path, "r") as f:
+                    data = json.load(f)
+                    matching_products = [name for name in data.keys() if re.search(product, name, re.IGNORECASE)]
+                    if matching_products:
+                        for name in matching_products:
+                            label = ttk.Label(result_frame, text=name)
+                            label.pack(side="top", padx=10, pady=10)
+                            label.bind("<Button-1>", lambda event, new_window=new_window, name=name: self.AddProduct(event, new_window, name))
+                    else:
+                        label = ttk.Label(result_frame, text="Brak wyników wyszukiwania. Dodaj produkt.")
+                        label.pack(side="top", padx=10, pady=10)
 
 
         entry.insert(0, "Wyszukaj produkt")
@@ -203,50 +222,55 @@ class App(tk.Tk):
 
 
         def show_file_content():
-            file_path = os.path.join(self.folder_path, name + ".txt")
+            file_path = os.path.join(self.folder_path, "Produkty.json")
             with open(file_path, 'r') as f:
-                content = f.read()
-                ProductInfo.config(text=content)
-            
+                data = json.load(f)
+                if name in data:
+                    content = json.dumps(data[name], indent=4)
+                    ProductInfo.config(text=content)
+
         ProductInfo.bind("<Button-1>", lambda event: show_file_content())
 
-        file_path = os.path.join(self.folder_path, name + ".txt")
-        with open(file_path, "r") as f:
-            lines = f.readlines()
-            for line in lines:
-                if line.find('Opakowanie') != -1:
-                    pack_g = [int(s) for s in re.findall(r'\b\d+\b', line)]
-                    pack_g = pack_g[0]
-                elif line.find('kcal') != -1:
-                    kcal_100g = [int(s) for s in re.findall(r'\b\d+\b', line)]
-                    kcal_100g = kcal_100g[0]
-                elif line.find('Tłuszcze:') != -1:
-                    Tłu_100g = [int(s) for s in re.findall(r'\b\d+\b', line)]
-                    Tłu_100g = Tłu_100g[0]
-                elif line.find('Węglowodany') != -1:
-                    Węgl_100g = [int(s) for s in re.findall(r'\b\d+\b', line)]
-                    Węgl_100g=Węgl_100g[0]
-                elif line.find('Białka') != -1:
-                    Biał_100g = [int(s) for s in re.findall(r'\b\d+\b', line)]
-                    Biał_100g=Biał_100g[0]
+        file_path = os.path.join(self.folder_path, "Produkty.json")
+        if os.path.exists(file_path):
+            with open(file_path, "r") as f:
+                data = json.load(f)
+                if name in data:
+                    values = data[name]
+                    pack_g = [int(s) for s in re.findall(r'\b\d+\b', str(values.get("Opakowanie ma", 0)))][0]
+                    kcal_100g = [int(s) for s in re.findall(r'\b\d+\b', str(values.get("Wartość energetyczna", 0)))][0]
+                    Tłu_100g = [int(s) for s in re.findall(r'\b\d+\b', str(values.get("Tłuszcze", 0)))][0]
+                    Węgl_100g = [int(s) for s in re.findall(r'\b\d+\b', str(values.get("Węglowodany", 0)))][0]
+                    Biał_100g = [int(s) for s in re.findall(r'\b\d+\b', str(values.get("Białka", 0)))][0]
         
         def display_selected(variable):
             option = variable.get()
             how_much = entry.get()
             if option == " Opakowanie ":
-                chosen_kcal = pack_g * kcal_100g / 100 * float(how_much)
+                self.chosen_kcal = pack_g * kcal_100g / 100 * float(how_much)
+                self.chosen_B = pack_g * Biał_100g / 100 * float(how_much)
+                self.chosen_T = pack_g * Tłu_100g / 100 * float(how_much)
+                self.chosen_W = pack_g * Węgl_100g / 100 * float(how_much)
             elif option == " g":
-                chosen_kcal = float(how_much) * kcal_100g / 100
-            label_chosen_kcal.config(text=str(chosen_kcal))
+                self.chosen_kcal = float(how_much) * kcal_100g / 100
+                self.chosen_B = float(how_much) * Biał_100g / 100
+                self.chosen_T = float(how_much) * Tłu_100g / 100
+                self.chosen_W = float(how_much) * Węgl_100g / 100
+            label_chosen_kcal.config(text=str(self.chosen_kcal)+" kcal")
         
         def print_selected_option():
             return (chosen_option.get())
 
-        chosen_kcal = 0
+        self.chosen_kcal = 0
+        self.chosen_B = 0
+        self.chosen_T = 0
+        self.chosen_W = 0
         chosen_option = tk.IntVar(value=2)
         validation = new_window.register(self.only_numbers)
         option_menu_list = ["", " Opakowanie ", " g"]
-        texts = [["1x Opakowanie", pack_g, pack_g*kcal_100g/100, ""], ["100 g", "100 g", kcal_100g, ""], ["200 g", "200 g", 2*kcal_100g, ""], ["entry", "choose", "chosen_kcal", ""]]
+        texts = [["1x Opakowanie", str(pack_g)+" g", str(pack_g*kcal_100g/100) + " kcal", ""], 
+                 ["100 g", "100 g", str(kcal_100g) + " kcal", ""], 
+                 ["200 g", "200 g", str(2*kcal_100g) + "kcal", ""], ["entry", "choose", "chosen_kcal", ""]]
         variable = tk.StringVar(value=option_menu_list[1])
         
         for i in range(len(texts)):
@@ -264,7 +288,7 @@ class App(tk.Tk):
                     optionmenu = ttk.OptionMenu(options_frame, variable, *option_menu_list, command=lambda choice: display_selected(variable))
                     optionmenu.grid(row=i, column=j, sticky="nsew")
                 elif texts[i][j] == "chosen_kcal":
-                    label_chosen_kcal = tk.Label(options_frame, text=str(chosen_kcal), height=2, width=15)
+                    label_chosen_kcal = tk.Label(options_frame, text=str(self.chosen_kcal) + " kcal", height=2, width=15)
                     label_chosen_kcal.grid(row=i, column=j)
                 else: 
                     label = tk.Label(options_frame, text=str(texts[i][j]), height=2, width=15)
@@ -272,15 +296,45 @@ class App(tk.Tk):
 
         def MacroProduct(chosen_option):
             if(chosen_option==0):
-                print(0)
+                self.product_kcal = pack_g * kcal_100g/100
+                self.product_B = pack_g * Biał_100g/100
+                self.product_T = pack_g * Tłu_100g/100
+                self.product_W = pack_g* Węgl_100g/100
             elif(chosen_option==1):
-                print(1)
+                self.product_kcal = kcal_100g
+                self.product_B = Biał_100g
+                self.product_T = Tłu_100g
+                self.product_W = Węgl_100g
             elif(chosen_option==2):
-                print(2)
+                self.product_kcal = 2*kcal_100g
+                self.product_B = 2*Biał_100g
+                self.product_T = 2*Tłu_100g
+                self.product_W = 2*Węgl_100g
             elif(chosen_option==3):
-                print(3)
+                self.product_kcal = self.chosen_kcal
+                self.product_B = self.chosen_B
+                self.product_T = self.chosen_T
+                self.product_W = self.chosen_W
 
-        SaveMeal = ttk.Button(new_window, text="Zapisz wybór",command=lambda: MacroProduct(print_selected_option()))
+            self.Makro[self.buttonID][0] += self.product_kcal
+            self.Makro[self.buttonID][1] += self.product_B
+            self.Makro[self.buttonID][2] += self.product_T
+            self.Makro[self.buttonID][3] += self.product_W
+
+            
+
+            self.treeview.insert(parent=self.buttonID, index="end", iid=None, text=name, 
+                                 values=(self.product_kcal, self.product_B, self.product_T, self.product_W))
+            self.treeview.item(self.buttonID, values=(self.Makro[self.buttonID][0], self.Makro[self.buttonID][1],
+                                            self.Makro[self.buttonID][2], self.Makro[self.buttonID][3]))
+
+            self.treeview.update()
+            self.update_macro_labels()
+            
+            
+
+        SaveMeal = ttk.Button(new_window, text="Zapisz wybór",
+                              command=lambda:[MacroProduct(print_selected_option()),self.on_closing(window,new_window)])
         SaveMeal.pack(side="bottom",padx=50, pady=0)
 
         new_window.protocol("WM_DELETE_WINDOW", lambda: self.on_closing(window,new_window))
@@ -302,7 +356,8 @@ class App(tk.Tk):
         frame1 = ttk.LabelFrame(new_window,text="Informacje")
         frame1.place(x=0,y=0,width=500,height=620)
 
-        self.label_texts = ["Nazwa","Wartości odżywcze na 100g:","Opakowanie ma", "Wartość energetyczna","Tłuszcze","Tłuszcze nasycone","Węglowodany","Cukry","Błonnik","Białka","Sól"]
+        self.label_texts = ["Nazwa","Wartości odżywcze na 100g:","Opakowanie ma", 
+                            "Wartość energetyczna","Tłuszcze","Tłuszcze nasycone","Węglowodany","Cukry","Błonnik","Białka","Sól"]
         self.label_texts2 = ["","","g/ml","kcal","g","g","g","g","g","g","g"]
 
 
@@ -330,9 +385,10 @@ class App(tk.Tk):
 
         new_window.protocol("WM_DELETE_WINDOW", lambda: self.on_closing(window,new_window))
 
+
+
     def SaveProduct(self, frame):
         filename = ""
-        entries = []
 
         for child in frame.winfo_children():
             if isinstance(child, tk.Label) and child["text"] == "Nazwa":
@@ -343,19 +399,37 @@ class App(tk.Tk):
                 break
 
         if filename != "":
-            file_path = os.path.join(self.folder_path, filename + ".txt")
+            file_path = os.path.join(self.folder_path, "Produkty.json")
+
+            if os.path.exists(file_path):
+                with open(file_path, "r") as f:
+                    try:
+                        data = json.load(f)
+                    except json.JSONDecodeError:
+                        data = {}
+            else:
+                data = {}
+
+            entries = {}
+
+            for child in frame.winfo_children():
+                if isinstance(child, tk.Label) and child["text"] != "Nazwa" and child["text"] in self.label_texts:
+                    text = child["text"]
+                    row_num = child.grid_info()["row"]
+                    for subchild2 in frame.winfo_children():
+                        if isinstance(subchild2, tk.Label) and subchild2["text"] in self.label_texts2 and subchild2.grid_info()["row"] == row_num:
+                            text2 = " " + subchild2["text"]
+                            for subchild in frame.winfo_children():
+                                if isinstance(subchild, tk.Entry) and subchild.master == child.master and subchild.grid_info()["row"] == row_num:
+                                    entries[text] = subchild.get() + text2
+                                    break
+
+            data[filename] = entries
+
             with open(file_path, "w") as f:
-                for child in frame.winfo_children():
-                    if isinstance(child, tk.Label) and child["text"] != "Nazwa" and child["text"] in self.label_texts:
-                        text = child["text"]
-                        row_num = child.grid_info()["row"]
-                        for subchild2 in frame.winfo_children():
-                            if isinstance(subchild2, tk.Label) and subchild2["text"] in self.label_texts2 and subchild2.grid_info()["row"] == row_num:
-                                text2=" "+subchild2["text"]
-                                for subchild in frame.winfo_children():
-                                    if isinstance(subchild, tk.Entry) and subchild.master == child.master and subchild.grid_info()["row"] == row_num:
-                                        f.write(text + ": " + subchild.get() + text2 +"\n")
-                                        break
+                json.dump(data, f, indent=4)
+
+
 
 
 
